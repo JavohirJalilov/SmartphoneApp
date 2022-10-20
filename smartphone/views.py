dcf9b36e47b17c2d1265b6b9eaa51a12d51213f5
Jalilov <javohirjalilov777@gmail.com>
2022-10-20 18:35:03 +0500

    add database

--- smartphone/views.py
+++ smartphone/views.py
@@ -1,6 +1,53 @@
 import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Product
 # Create your views here.
 def home(request):
     return JsonResponse({"PAGE":"HOME"})
+def convert_to_json(product):
+    """
+    Convert a product to a JSON object
+    args:
+        product: a product object
+    returns:
+        a JSON object
+    """
+    product_json = {
+        'id': product.id,
+        'name': product.name,
+        'company': product.company,
+        'color': product.color,
+        'RAM': product.RAM,
+        'memory': product.memory,
+        'price': product.price,
+        'created_at': product.created_at,
+        'updated_at': product.updated_at,
+        'img_url': product.img_url,
+    }
+    return product_json
+    
+def add_product(request):
+    """
+    Create a product
+    args:
+        request: the request object
+    return:
+        JsonResponse: the product
+    """
+    if request.method == 'POST':
+        product = Product.objects.create(
+            name=request.POST['name'],
+            company=request.POST['company'],
+            color=request.POST['color'],
+            RAM=request.POST['RAM'],
+            memory=request.POST['memory'],
+            price=request.POST['price'],
+            img_url=request.POST['img_url'],
+        )
+        product_json = convert_to_json(product)
+        product.save() # save the product to the database
+        
+        return JsonResponse({'product': product_json})
+
+    return JsonResponse({'product': {}})
