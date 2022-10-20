43a74ab6596166e49c61e53a0810efad1809655b
Jalilov <javohirjalilov777@gmail.com>
2022-10-20 18:05:12 +0500

    home page

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse, JsonResponse
 from django.urls import path
+def home():
+    JsonResponse({"OK":True})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home)
 ]
